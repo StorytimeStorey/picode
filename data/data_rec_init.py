6a031447b66d3ba915d4b0ef212fe2349d3f3147
StorytimeStorey <tomichaels@hotmail.com>
2023-03-26 12:15:05 -0700

    fixed filenames

--- data/data_rec_init.py
+++ data/data_rec_init.py
@@ -3,7 +3,7 @@ from data.current_sensor import Sensor
 class TemperatureAndPressure:
     def __init__(self):
         self.sensor = Sensor()
-        self.csv = 'Data/CSV/raw.csv'
+        self.csv = 'data/csv/raw.csv'
 
     def record_data_to_csv(self):
         self.sensor.update_readings()
