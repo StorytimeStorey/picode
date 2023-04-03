022c63398a0909aad1beb74243736bff4b547617
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2023-04-02 18:27:31 -0700

    Update start.py

--- start.py
+++ start.py
@@ -5,5 +5,5 @@ import subprocess
 #This will run all the scripts that we need to run at the same time
 #The bot, the data collector, eventually the alerts checker and maybe the processes for running the relays
 subprocess.Popen(['python', 'data/start_data.py'])
-subprocess.Popen(['python', 'bot/Bonnibel.py'])
+#subprocess.Popen(['python', 'bot/Bonnibel.py'])
 subprocess.Popen(['python', 'bot_2/McJohnson.py'])
