e7fb2b4382cd8811ebfd44dfac272641485d1b5b
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2023-04-18 18:27:06 -0700

    Changed McJohnosn

--- bot/bot.py
+++ bot/bot.py
@@ -26,24 +26,20 @@ intents = discord.Intents(value=7,
                           typing=True,
                           integrations=True)
 
-Client = discord.Client(intents=intents)
-
-# channel = Client.get_channel(channel_id)
-
 # set command prefix
-McJohnson = commands.Bot(command_prefix='@M', intents=intents)
+bot = commands.Bot(command_prefix='@M', intents=intents)
 
 #Not sure what this command is doing. Jackson, any comments here?
-@McJohnson.command(name='set_channel')
+@bot.command(name='set_channel')
 async def set_channel(ctx):
-    McJohnson.default_channel = ctx.channel
+    bot.default_channel = ctx.channel
     await ctx.send('Channel set.')
 
-@McJohnson.hybrid_command()
+@bot.hybrid_command()
 async def hello(ctx):
     await ctx.send("hello!")
 
-@McJohnson.hybrid_command()
+@bot.hybrid_command()
 async def print_graphs(ctx): #This needs to be connected to a pipe in order to work properly, since it takes too long to work normally.
     if os.path.exists(pi_johnson_path):
         current_day = datetime.today().strftime('%d_%m_%y')
@@ -71,9 +67,9 @@ async def print_graphs(ctx): #This needs to be connected to a pipe in order to w
 @tasks.loop(seconds=30)
 async def change_status():
     current_status = info.read_last_row('controller/data/csv/raw.csv')
-    await McJohnson.change_presence(activity=discord.Game(f"{current_status}"))
+    await bot.change_presence(activity=discord.Game(f"{current_status}"))
 
-@McJohnson.hybrid_command()
+@bot.hybrid_command()
 async def print_status(ctx):
     if os.path.exists(pi_johnson_path):
         try:
@@ -90,47 +86,46 @@ async def print_status(ctx):
     else: #CODE FOR TESTING ENVIRONMENT
         current_status = info.read_last_row('controller/data/csv/test.csv')
         await ctx.send(f"{current_status}")
-@McJohnson.hybrid_command()
+@bot.hybrid_command()
 # @commands.is_owner()
 async def shutdown(ctx):
     exit()
     quit()
 
-# @McJohnson.event
-# async def send_alert(channel, alert_csv):
-#     last_row = alert_csv.iloc[-1]
-#     date = last_row[0]
-#     time = last_row[1]
-#     temp = last_row[2]
-#     humidity = last_row[3]
-#     alert_type = last_row[4]
-#     heater_status = last_row[5]
-#     ac_status = last_row[6]
-#     humidifier_status = last_row[7]
-#     await channel.send(f'On {date} at {time}, the following alert was found:\nAlert Type: {alert_type}\nTemperature: {temp}\nHumidity: {humidity}\nHeater Status: {heater_status}\nAC Status: {ac_status}\nHumidifier Status: {humidifier_status}')
-# # loop to check if it needs to send an alert or not
-# @tasks.loop(seconds=5)
-# async def alert(channel):
-#     # if len_alerts exists, check if the length has changed and send alert if it has. if it does not exist, create it
-#     alert_csv = pd.read_csv('controller/data/csv/alerts.csv')
-#     if not 'len_alerts' in globals():
-#         global len_alerts
-#         len_alerts = len(alert_csv)
-#         print(len_alerts)
-#     else:
-#         if len(alert_csv) > len_alerts:
-#             await send_alert(channel, alert_csv)
-#             print('sent alert')
-    # write_alert()
+@bot.event
+async def send_alert(channel, alert_csv):
+    last_row = alert_csv.iloc[-1]
+    date = last_row[0]
+    time = last_row[1]
+    temp = last_row[2]
+    humidity = last_row[3]
+    alert_type = last_row[4]
+    heater_status = last_row[5]
+    ac_status = last_row[6]
+    humidifier_status = last_row[7]
+    await channel.send(f'On {date} at {time}, the following alert was found:\nAlert Type: {alert_type}\nTemperature: {temp}\nHumidity: {humidity}\nHeater Status: {heater_status}\nAC Status: {ac_status}\nHumidifier Status: {humidifier_status}')
+# loop to check if it needs to send an alert or not
+@tasks.loop(seconds=5)
+async def alert(channel):
+    # if len_alerts exists, check if the length has changed and send alert if it has. if it does not exist, create it
+    alert_csv = pd.read_csv('controller/data/csv/alerts.csv')
+    if not 'len_alerts' in globals():
+        global len_alerts
+        len_alerts = len(alert_csv)
+    else:
+        if len(alert_csv) > len_alerts:
+            await send_alert(channel, alert_csv)
+            print('sent alert')
+    write_alert()
 
-    # current_status = info.read_last_row('data/csv/test.csv')
-    # await channel.send(f'{current_status}')
+    current_status = info.read_last_row('data/csv/test.csv')
+    await channel.send(f'{current_status}')
 
 # syncs the task tree and start the alerts loop
-@McJohnson.event
+@bot.event
 async def on_ready():
-    await McJohnson.tree.sync()
-    channel = McJohnson.get_channel(channel_id)
+    await bot.tree.sync()
+    channel = bot.get_channel(channel_id)
     # alert.start(channel)
     # exit()
 
@@ -147,7 +142,7 @@ def run_bot():
     else:
         print("Failed to find token") #Catches for failures...
 
-    McJohnson.run(token)
+    bot.run(token)
 
 if __name__ == '__main__':
     run_bot()
